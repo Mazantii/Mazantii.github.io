fe224baeebd7c5e2a46355da9a35c6a9a9ac0893
Mazantii <90619558+Mazantii@users.noreply.github.com>
2022-12-07 11:29:08 +0100

    skrabelod og animationer

--- Resources/javascript/js.js
+++ Resources/javascript/js.js
@@ -1,25 +1,82 @@
+//skrabelod 1
 var card = document.getElementById("scratch"),
     cardCanvas = card.getContext('2d'),
     brushRadius = (card.width / 50),
     img = new Image();
 
+//skrabelod 2
+var card2 = document.getElementById("scratch2"),
+    cardCanvas2 = card2.getContext('2d'),
+    brushRadius2 = (card2.width / 50),
+    img2 = new Image();
+
+//skrabelod 3
+var card3 = document.getElementById("scratch3"),
+    cardCanvas3 = card3.getContext('2d'),
+    brushRadius3 = (card3.width / 50),
+    img3 = new Image();
+
 var scrollscript;
 
+//Skrabelod 1
 if (brushRadius < 1) { brushRadius = 1 }
 
+//Skrabelod 2
+if (brushRadius2 < 1) { brushRadius2 = 1 }
+
+//skabelod 3
+if (brushRadius3 < 1) { brushRadius3 = 1 }
+
+//skrabelod 1
 img.onload = function(){  
     cardCanvas.drawImage(img, 0, 0, card.width, card.height);
 }
 
+//skrabelod 2
+img2.onload = function(){  
+    cardCanvas2.drawImage(img2, 0, 0, card2.width, card2.height);
+}
+
+//skrabelod 3
+img3.onload = function(){  
+    cardCanvas3.drawImage(img3, 0, 0, card3.width, card3.height);
+}
+
+//Skrabelod 1
 img.loc = 'img/';
 img.filename = 'skrabelod/skrabelodaldersgrænse2.png';
 
+//skrabelod 2
+img2.loc = 'img/';
+img2.filename = 'skrabelod/skrabelodb&u2.png';
+
+//skrabelod 3
+img3.loc = 'img/';
+img3.filename = 'skrabelod/skrabelodmen2.png';
+
+//skrabelod 1
 if (window.devicePixelRatio >= 2) {
   var nameParts = img.filename.split('.');
   img.src = img.loc + nameParts[0]+"-2x"+"."+nameParts[1];
 } else {
   img.src = img.loc + img.filename;
 }
+
+//skrabelod 2
+if (window.devicePixelRatio >= 2) {
+    var nameParts2 = img2.filename.split('.');
+    img2.src = img2.loc + nameParts2[0]+"-2x"+"."+nameParts2[1];
+  } else {
+    img2.src = img2.loc + img2.filename;
+  }
+
+  //skrabelod 3
+if (window.devicePixelRatio >= 2) {
+    var nameParts3 = img3.filename.split('.');
+    img3.src = img3.loc + nameParts3[0]+"-2x"+"."+nameParts3[1];
+  } else {
+    img3.src = img3.loc + img3.filename;
+  }
 
 function detectLeftButton(event) {
     if ('buttons' in event) {
@@ -31,6 +88,8 @@ function detectLeftButton(event) {
     }
 }
 
+
+//skrabelod 1
 function getBrushPos(xRef, yRef) {
   var cardRect = card.getBoundingClientRect();
     return {
@@ -48,19 +107,87 @@ function drawDot(mouseX,mouseY){
 }
 
 card.addEventListener("mousemove", function(e) {
-  var brushPos = getBrushPos(e.clientX, e.clientY);
-  var leftBut = detectLeftButton(e);
-  if (leftBut == 1) {
-    drawDot(brushPos.x, brushPos.y);
+    var brushPos = getBrushPos(e.clientX, e.clientY);
+    var leftBut = detectLeftButton(e);
+    if (leftBut == 1) {
+      drawDot(brushPos.x, brushPos.y);
+    }
+  }, false);
+  
+  card.addEventListener("touchmove", function(e) {
+      e.preventDefault();
+      var touch = e.targetTouches[0];
+      if (touch) {
+      var brushPos = getBrushPos(touch.pageX, touch.pageY);
+          drawDot(brushPos.x, brushPos.y);
+      }
+  }, false);
+
+//skrabelod 2
+function getBrushPos2(xRef, yRef) {
+    var cardRect2 = card2.getBoundingClientRect();
+      return {
+      x: Math.floor((xRef-cardRect2.left)/(cardRect2.right-cardRect2.left)*card2.width),
+      y: Math.floor((yRef-cardRect2.top)/(cardRect2.bottom-cardRect2.top)*card2.height)
+      };
+  }
+        
+  function drawDot2(mouseX,mouseY){
+      cardCanvas2.beginPath();
+      cardCanvas2.arc(mouseX, mouseY, brushRadius2, 0, 2*Math.PI, true);
+      cardCanvas2.fillStyle = '#000';
+      cardCanvas2.globalCompositeOperation = "destination-out";
+      cardCanvas2.fill();
+  }
+
+card2.addEventListener("mousemove", function(e) {
+  var brushPos2 = getBrushPos2(e.clientX, e.clientY);
+  var leftBut2 = detectLeftButton(e);
+  if (leftBut2 == 1) {
+    drawDot2(brushPos2.x, brushPos2.y);
   }
 }, false);
 
-card.addEventListener("touchmove", function(e) {
+card2.addEventListener("touchmove", function(e) {
     e.preventDefault();
-    var touch = e.targetTouches[0];
-    if (touch) {
-    var brushPos = getBrushPos(touch.pageX, touch.pageY);
-        drawDot(brushPos.x, brushPos.y);
+    var touch2 = e.targetTouches[0];
+    if (touch2) {
+    var brushPos2 = getBrushPos2(touch2.pageX, touch2.pageY);
+        drawDot2(brushPos2.x, brushPos2.y);
+    }
+}, false);
+
+//skrabelod 3
+function getBrushPos3(xRef, yRef) {
+    var cardRect3 = card3.getBoundingClientRect();
+      return {
+      x: Math.floor((xRef-cardRect3.left)/(cardRect3.right-cardRect3.left)*card3.width),
+      y: Math.floor((yRef-cardRect3.top)/(cardRect3.bottom-cardRect3.top)*card3.height)
+      };
+  }
+        
+  function drawDot3(mouseX,mouseY){
+      cardCanvas3.beginPath();
+      cardCanvas3.arc(mouseX, mouseY, brushRadius3, 0, 2*Math.PI, true);
+      cardCanvas3.fillStyle = '#000';
+      cardCanvas3.globalCompositeOperation = "destination-out";
+      cardCanvas3.fill();
+  }
+
+card3.addEventListener("mousemove", function(e) {
+  var brushPos3 = getBrushPos3(e.clientX, e.clientY);
+  var leftBut3 = detectLeftButton(e);
+  if (leftBut3 == 1) {
+    drawDot3(brushPos3.x, brushPos3.y);
+  }
+}, false);
+
+card3.addEventListener("touchmove", function(e) {
+    e.preventDefault();
+    var touch3 = e.targetTouches[0];
+    if (touch3) {
+    var brushPos3 = getBrushPos3(touch3.pageX, touch3.pageY);
+        drawDot3(brushPos3.x, brushPos3.y);
     }
 }, false);
 
@@ -90,62 +217,82 @@ let processScroll = () => {
     console.log(scrollscript);
 
     //Her fikser vi animationer
-    if(scrollscript > 1.5 && scrollscript < 3.2 ) {
+    if(scrollscript > 1.4 && scrollscript < 3.2 ) {
         document.getElementById('mont').style.setProperty('transform', 'scale(200%)');
 
     } else{
         document.getElementById('mont').style.setProperty('transform', 'scale(150%)');
     }
 
-    if(scrollscript > 5.6 && scrollscript < 8.2 ) {
+    if(scrollscript > 4.5 && scrollscript < 5.5 ) {
+        document.getElementById('scratch').style.setProperty('transform', 'scale(120%)');
+
+    } else{
+        document.getElementById('scratch').style.setProperty('transform', 'scale(100%)');
+    }
+
+    if(scrollscript > 5.6 && scrollscript < 9.2 ) {
         document.getElementById('mobil').style.setProperty('transform', 'scale(100%)');
 
     } else{
         document.getElementById('mobil').style.setProperty('transform', 'scale(80%)');
     }
 
-    if(scrollscript > 9.6 && scrollscript < 11.5 ) {
+    if(scrollscript > 9.6 && scrollscript < 12 ) {
         document.getElementById('kantine').style.setProperty('transform', 'scale(200%)');
 
     } else{
         document.getElementById('kantine').style.setProperty('transform', 'scale(150%)');
     }
 
-    if(scrollscript > 12.2 && scrollscript < 14.3 ) {
+    if(scrollscript > 12.2 && scrollscript < 15.3 ) {
         document.getElementById('til4').style.setProperty('transform', 'scale(300%)');
 
     } else{
         document.getElementById('til4').style.setProperty('transform', 'scale(200%)');
     }
 
-    if(scrollscript > 15.4 && scrollscript < 16.3 ) {
-        document.getElementById('mellem').style.setProperty('transform', 'scale(110%)');
-
-    } else{
-        document.getElementById('mellem').style.setProperty('transform', 'scale(90%)');
-    }
-
-    if(scrollscript > 17.5 && scrollscript < 20.3 ) {
+    if(scrollscript > 15.8 && scrollscript < 19 ) {
         document.getElementById('far').style.setProperty('transform', 'scale(200%)');
 
     } else{
         document.getElementById('far').style.setProperty('transform', 'scale(150%)');
     }
 
-    if(scrollscript > 21.1 && scrollscript < 22.2 ) {
+    if(scrollscript > 19.3 && scrollscript < 21 ) {
         document.getElementById('infogram-mennesker').style.setProperty('transform', 'scale(120%)');
 
     } else{
         document.getElementById('infogram-mennesker').style.setProperty('transform', 'scale(100%)');
     }
 
-    if(scrollscript > 23 && scrollscript < 24.2 ) {
+    if(scrollscript > 21.3 && scrollscript < 22.6 ) {
         document.getElementById('Henrik').style.setProperty('transform', 'scale(170%)');
 
     } else{
         document.getElementById('Henrik').style.setProperty('transform', 'scale(110%)');
     }
 
+    if(scrollscript > 28.8 && scrollscript < 31 ) {
+        document.getElementById('fiske').style.setProperty('transform', 'scale(150%)');
+
+    } else{
+        document.getElementById('fiske').style.setProperty('transform', 'scale(100%)');
+    }
+
+    if(scrollscript > 32 && scrollscript < 34.3 ) {
+        document.getElementById('fanget').style.setProperty('transform', 'scale(150%)');
+
+    } else{
+        document.getElementById('fanget').style.setProperty('transform', 'scale(100%)');
+    }
+
+    if(scrollscript > 32 && scrollscript < 34.3 ) {
+        document.getElementById('smaat').style.setProperty('transform', 'scale(150%)');
+
+    } else{
+        document.getElementById('smaat').style.setProperty('transform', 'scale(100%)');
+    }
 }
 
 /*Et forsøg på at gøre det samme som koden for oven, men med en enkelt container...
